4dd1d68c7c3d3ffc9602ff837908f9bf61587067
rohit9869 <138598042+rohit9869@users.noreply.github.com>
2026-03-15 23:18:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -88,6 +88,8 @@ def predict():
         top3=top3
     )
 
+import os
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=False)
